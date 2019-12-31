a070a1874784296a7ef8263b8b65723c3fbc976e
TAEWOO KIM <taewookimmr@gmail.com>
2020-01-01 04:56:14 +0900

    project copied to another dir

---
+++ mobile_server_programming/Chapter3/monTester/src/mon_app.erl
@@ -0,0 +1,62 @@
+%%%-------------------------------------------------------------------
+%%% @author taewookim
+%%% @copyright (C) 2020, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 01. 1월 2020 오전 3:15
+%%%-------------------------------------------------------------------
+-module(mon_app).
+-author("taewookim").
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2,
+  stop/1]).
+
+%%%===================================================================
+%%% Application callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called whenever an application is started using
+%% application:start/[1,2], and should start the processes of the
+%% application. If the application is structured according to the OTP
+%% design principles as a supervision tree, this means starting the
+%% top supervisor of the tree.
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(start(StartType :: normal | {takeover, node()} | {failover, node()},
+    StartArgs :: term()) ->
+  {ok, pid()} |
+  {ok, pid(), State :: term()} |
+  {error, Reason :: term()}).
+start(_StartType, _StartArgs) ->
+  case 'mon_sup':start_link() of
+    {ok, Pid} ->
+      io:format("start but something differnt from the textbook~n"),
+      {ok, Pid};
+    Error ->
+      Error
+  end.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called whenever an application has stopped. It
+%% is intended to be the opposite of Module:start/2 and should do
+%% any necessary cleaning up. The return value is ignored.
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(stop(State :: term()) -> term()).
+stop(_State) ->
+  ok.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
