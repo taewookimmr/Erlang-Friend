0b50b94438ce5755c92e766b564b4a142bd16251
taewookim <taewookim@taewookimui-MacBookAir.local>
2020-01-06 18:35:21 +0900

    docs:Add beam files

--- erlang_armstrong/11_IRC_Lite/socket_dist/chat_client.erl
+++ erlang_armstrong/11_IRC_Lite/socket_dist/chat_client.erl
@@ -1,6 +1,6 @@
 -module(chat_client).
 
--import(io_widget,[get_state/1, insert_str/2, set_prompt/2, set_state/2, set_title/2, set_handle/2, update_state/3]).
+-import(io_widget,[get_state/1, insert_str/2, set_prompt/2, set_state/2, set_title/2, set_handler/2, update_state/3]).
 
 -export([start/0, test/0, connect/5]).
 
